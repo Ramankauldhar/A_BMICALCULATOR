394521c679ee749950c3699343d9de0f555cd04a
Ramankauldhar <55930003+Ramankauldhar@users.noreply.github.com>
2019-12-12 13:43:30 -0500

    V9: Printted the BMI Message (All conditions done)

--- ViewController.swift
+++ ViewController.swift
@@ -26,11 +26,46 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var ResultLabel: UILabel!
     
+    @IBOutlet weak var BMIMessageLabel: UILabel!
+    
     @IBAction func BMICalculateButton(_ sender: UIButton) {
         height = Double(User_Height.text!) as! Double
         weight = Double(User_Weight.text!) as! Double
         result = (weight*703)/(height*height)
         ResultLabel.text = String(format:"%.2f",result)
+        
+        if(result < 16)
+        {
+            BMIMessageLabel.text = String("Severe Thinness")
+        }
+        if(result >= 16 && result <= 17)
+        {
+            BMIMessageLabel.text = String("Moderate Thinness")
+        }
+        if(result >= 17 && result <= 18.5)
+        {
+            BMIMessageLabel.text = String("Mild Thinness")
+        }
+        if(result >= 18.5 && result <= 25)
+        {
+            BMIMessageLabel.text = String("Normal")
+        }
+        if(result >= 25 && result <= 30)
+        {
+            BMIMessageLabel.text = String("OverWeight")
+        }
+        if(result >= 30 && result <= 35)
+        {
+            BMIMessageLabel.text = String("Obese Class 1")
+        }
+        if(result >= 35 && result <= 40)
+        {
+            BMIMessageLabel.text = String("Obese Class 2")
+        }
+        if(result > 40)
+        {
+            BMIMessageLabel.text = String("Obese Class 3")
+        }
     }
     
     
