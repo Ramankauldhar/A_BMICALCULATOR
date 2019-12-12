eb29aacf40195f558359e4de51681e16532ba766
Ramankauldhar <55930003+Ramankauldhar@users.noreply.github.com>
2019-12-11 20:38:07 -0500

    V2: BMI Calculate Coding

--- ViewController.swift
+++ ViewController.swift
@@ -8,16 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//initializing the variables
+private var heightt: Double = 0.0
+private var weight: Double = 0.0
+private var result: Double = 0.0
 
+class ViewController: UIViewController {
     
+    //connecting the UIFields
     @IBOutlet weak var Username: UITextField!
     @IBOutlet weak var UserAge: UITextField!
     @IBOutlet weak var UserGender: UITextField!
     @IBOutlet weak var UserHeight: UITextField!
     @IBOutlet weak var UserWeight: UITextField!
+    @IBOutlet weak var resultlabel: UILabel!
     
-    @IBOutlet weak var CalculateResult: UITextField!
+    @IBOutlet weak var doneButton: UIButton!
+    
+    //saving the value
+    heightt = Double(UserHeight.text!) as! Double
+    weight = Double(UserWeight.text!) as! Double
+    result = (weight*703)/(height*height)
+    resultlabel.text = String(result)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
