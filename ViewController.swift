524a21380f9ac3c5b244ad18af75c607e024394b
Ramankauldhar <55930003+Ramankauldhar@users.noreply.github.com>
2019-12-11 20:05:15 -0500

    V1: Usi is Created

---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BMIAPPTEST
+//
+//  Created by SETAS on 2019-12-11.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var Username: UITextField!
+    @IBOutlet weak var UserAge: UITextField!
+    @IBOutlet weak var UserGender: UITextField!
+    @IBOutlet weak var UserHeight: UITextField!
+    @IBOutlet weak var UserWeight: UITextField!
+    
+    @IBOutlet weak var CalculateResult: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
+
